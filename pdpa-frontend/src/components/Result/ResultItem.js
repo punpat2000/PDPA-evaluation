648026b3d2d3a5d496a827df7a467135b66f9cfa
Pattradanai Punvichartkul <saint12302@gmail.com>
2022-04-28 05:58:03 +0700

    add results

---
+++ pdpa-frontend/src/components/Result/ResultItem.js
@@ -0,0 +1,5 @@
+const ResultItem = () => {
+
+}
+
+export default ResultItem;
---
+++ pdpa-frontend/src/components/Result/Results.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { MOCK_DATA } from 'mock-data';
+import styles from 'Result.module.css';
+
+const Results = () => {
+	const [results, setResults] = useState(MOCK_DATA);
+
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default Results;
