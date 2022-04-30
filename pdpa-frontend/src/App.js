16759af3c8f7216382667e8dcab8adbfe300d441
Pattradanai Punvichartkul <saint12302@gmail.com>
2022-05-01 02:12:12 +0700

    update app.js

--- pdpa-frontend/src/App.js
+++ pdpa-frontend/src/App.js
@@ -1,42 +1,22 @@
-import { Fragment } from 'react';
+import { lazy, Suspense } from 'react';
 import './App.css';
 import InputForm from './components/Input/InputForm';
-import Results from './components/Result/Results';
-
-// const DUMMY_EXPENSES = [
-// 	{
-// 		id: 'e1',
-// 		title: 'Toilet Paper',
-// 		amount: 94.12,
-// 		date: new Date(2020, 7, 14),
-// 	},
-// 	{ id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-// 	{
-// 		id: 'e3',
-// 		title: 'Car Insurance',
-// 		amount: 294.67,
-// 		date: new Date(2021, 2, 28),
-// 	},
-// 	{
-// 		id: 'e4',
-// 		title: 'New Desk (Wooden)',
-// 		amount: 450,
-// 		date: new Date(2021, 5, 12),
-// 	},
-// ];
+// import Results from './components/Result/Results';
 
 function App() {
-	// const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-
 	const submitHandler = (data) => {
 		console.log('form recevied, from app');
 		console.log(JSON.stringify(data));
 	};
 
+	const LazyResults = lazy(() => import('./components/Result/Results'));
+
 	return (
 		<>
 			<InputForm onSubmit={submitHandler} />
-			<Results />
+			<Suspense fallback={<div>Loading...</div>}>
+				<LazyResults />
+			</Suspense>
 		</>
 	);
 }
