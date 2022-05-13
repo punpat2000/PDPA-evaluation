94e975ae70a3faa916fbf17080c8dbeaf3716b50
Pattradanai Punvichartkul <saint12302@gmail.com>
2022-05-14 05:34:40 +0700

    adjust content layout

--- pdpa-frontend/src/components/Result/ResultList.js
+++ pdpa-frontend/src/components/Result/ResultList.js
@@ -15,10 +15,12 @@ const ResultList = (props) => {
 			{props.items.map((res) => {
 				return (
 					<li className={`padding-rule ${styles['result-card']}`} key={res.id}>
-						<span>{res.content}</span>
-						<br />
-						<small className={styles.timestamp}>{res.date}</small>
-						<Status labels={res.labels} />
+						<section>
+							<span className={styles.sentense}>{res.content}</span>
+							<br />
+							<small className={styles.timestamp}>{res.date}</small>
+							<Status labels={res.labels} />
+						</section>
 					</li>
 				);
 			})}
