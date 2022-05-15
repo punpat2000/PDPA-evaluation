f124e20da2d8ef1a74b522c61e772c1ab061636b
Pattradanai Punvichartkul <saint12302@gmail.com>
2022-05-16 01:21:34 +0700

    add error handler

--- pdpa-frontend/src/App.js
+++ pdpa-frontend/src/App.js
@@ -4,8 +4,10 @@ import InputForm from './components/Input/InputForm';
 import Results from './components/Result/Results';
 import LoadingUI from './components/UI/Loading';
 import { MOCK_DATA } from './components/Result/mock-data';
+import ErrorModal from './components/UI/ErrorModal';
 
 function App() {
+	const [isError, setIsError] = useState(false);
 	const [isLoading, setLoad] = useState(false);
 	const [isAnalyzed, setAnalyze] = useState(false);
 	const [analyzedData, setData] = useState(MOCK_DATA);
@@ -24,15 +26,33 @@ function App() {
 
 	const submitHandler = async (data) => {
 		setLoad(true);
-		const res = await fetchData(data);
-		console.log(res);
-		setData(res);
-		setAnalyze(true);
+		try {
+			console.log(data);
+			const res = await fetchData(data);
+			console.log(res);
+			setData(res);
+			setAnalyze(true);
+		} catch (e) {
+			setIsError(true);
+			console.log(e.name);
+		}
 		setLoad(false);
+	};
+
+	const errorHandler = () => {
+		setIsError(false);
 	};
 
 	return (
 		<>
+			{isError && (
+				<ErrorModal
+					title={'error'}
+					message="An error occurred. Please try again later."
+					onConfirm={errorHandler}
+					isError={isError}
+				/>
+			)}
 			{isLoading && <LoadingUI />}
 			{!isAnalyzed ? (
 				<InputForm onSubmit={submitHandler} />
