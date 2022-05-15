89fe8c242fd0d405a522f9dbe7c42f6c9c6aff1b
Pitawat Sangpaiboon <51756741+xlububankx@users.noreply.github.com>
2022-05-16 02:26:01 +0700

    fix no post padding

--- pdpa-frontend/src/components/Result/ResultList.js
+++ pdpa-frontend/src/components/Result/ResultList.js
@@ -1,31 +1,33 @@
-import styles from './Result.module.css';
-import Status from './Status';
+import styles from "./Result.module.css";
+import Status from "./Status";
 
 const ResultList = (props) => {
-	if (props.items.length === 0) {
-		return (
-			<div className={styles['result-card']}>
-				<h2 className="expenses-list__fallback">Found No Posts</h2>
-			</div>
-		);
-	}
+  if (props.items.length === 0) {
+    return (
+      <div className="padding-rule">
+        <div className={styles["result-card"]}>
+          <h2 className="expenses-list__fallback">Found no posts</h2>
+        </div>
+      </div>
+    );
+  }
 
-	return (
-		<ul className={`${styles['no-bullets']} ${styles.appear}`}>
-			{props.items.map((res) => {
-				return (
-					<li className={`padding-rule ${styles['result-card']}`} key={res.id}>
-						<section>
-							<span className={styles.sentense}>{res.content}</span>
-							<br />
-							<small className={styles.timestamp}>{res.date}</small>
-							<Status labels={res.labels} />
-						</section>
-					</li>
-				);
-			})}
-		</ul>
-	);
+  return (
+    <ul className={`${styles["no-bullets"]} ${styles.appear}`}>
+      {props.items.map((res) => {
+        return (
+          <li className={`padding-rule ${styles["result-card"]}`} key={res.id}>
+            <section>
+              <span className={styles.sentense}>{res.content}</span>
+              <br />
+              <small className={styles.timestamp}>{res.date}</small>
+              <Status labels={res.labels} />
+            </section>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default ResultList;
